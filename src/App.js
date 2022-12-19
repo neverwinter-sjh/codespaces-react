baaf87e6d328cdee2869792c480651123cd4ac67
Jaeho Sim <neverwinter.sjh@gmail.com>
2022-12-19 04:34:03 +0000

    ㅇㅇ

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import './App.css';
+import { useRef } from "react";
 
 function App() {
+  const ref = useRef(null);
+
+  const handleChange = event => {
+    if (event?.target?.files && event?.target?.files.length > 0) {
+      const file = event?.target?.files[0];
+      const fileSize = file.size / 1024 / 1024;
+      if (fileSize > 10) {
+        alert("파일 크기가 10MB를 초과합니다.");
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <input type="file" ref={ref} onChange={e => handleChange(e)} />
     </div>
   );
 }
